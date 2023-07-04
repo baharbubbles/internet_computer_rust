0e9e70578e085f468e6fe89771b018d53686468c
baharbubbles <baharirmak222@gmail.com>
2023-07-05 00:58:13 +0300

    calculator taski

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,72 @@
+use std::io;
+
+enum Operations{
+    Add{x: f64, y: f64},
+    Subtract{x: f64, y: f64},
+    Multiply{x: f64, y: f64},
+    Divide{x: f64, y: f64},
+}
+impl Operations {
+fn calculate (&self) -> f64 {
+    match self {
+        Operations::Add {x, y} => {
+            return x+y;
+        }
+        Operations::Subtract {x, y} => {
+            return x-y;
+        }
+        Operations::Multiply {x, y} => {
+            return x*y;
+        }
+        Operations::Divide {x, y} => {
+            return x/y;
+        }
+    }
+}
+}
+
 fn main() {
-    let message="Hello, World!";
-    // x-değişken_adı: i32-değişken_tipi = 42-değişkenin_değeri
-    let x: i32 = 42;
-    let pi: f64 = 3.14;
-    let is_rust_fun: bool = true;
-    let letter_a: char = 'a';
+    let mut x = String::new();
+    let mut opr = String::new(); 
+    let mut y = String::new();
 
-    // fonksiyon oluşturma
-    // add isimli fonksiyona i32 titpnde iki parametre eklendi, "-> i32" de fonksiyonun return'ü
-    fn add(x: i32, y: i32) -> i32 {
-        x+y
-    }
-    
-    // karşılaştırma yapma
-    let x = 42; //x'e değer verildi
-    if x >= 0 {
-        println!("x is non-negative");  // println! şeklinde yazılıyor dikkat!!!
-    }
-    else{
-        println!("x is negative");
+    println!("Input First Number");
+    io::stdin().read_line(&mut x).expect("failed to read input");
+    let x: f64 = x.trim().parse().expect("invalid input");
+
+    println!("Input Operation(+,-,*,/)");
+    io::stdin().read_line(&mut opr).expect("failed to read input");
+    let opr: char = opr.trim().parse().expect("invalid input");
+
+    println!("Input Second Number");
+    io::stdin().read_line(&mut y).expect("failed to read input");
+    let y: f64 = y.trim().parse().expect("invalid input");
+
+    println!("{:?} {:?} {:?} = ",x,opr,y);
+
+     match opr {
+        '+' => {
+        let islem=  Operations::Add {x:x, y: y};
+         println!("{:?}",islem.calculate());
+
+        }
+
+        '-' => {
+        let islem= Operations::Subtract {x:x, y: y};
+        println!("{:?}",islem.calculate());
+        }
+
+        '*' => {
+        let islem= Operations::Multiply {x:x, y: y};
+        println!("{:?}",islem.calculate());
+        }
+
+        '/' => {
+        let islem=   Operations::Divide {x:x, y: y};
+        println!("{:?}",islem.calculate());
+        }
+        _ => unreachable!()
     }
 
-    // döngü yapma
-    let mut i = 1; //mutable yani güncellenebilen değişken old. belirTiyor
-    while i <= 5 {
-        println!("{}", i);
-        i += 1;
-    }
 
 }
-// ** genel olarak syntax yapısı javascript ve c diline çok benzer **
